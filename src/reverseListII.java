9ac5af1aeefa837b226b9402708ff3ec8b1c9f7c
nfx_git <13561592791@163.com>
2021-09-21 19:34:27 +0800

    Initial commit

---
+++ src/reverseListII.java
@@ -0,0 +1,39 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class reverseListII {
+    ListNode nextNode = null;
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == 1) {
+            return reverseN(head, right);
+        }
+        head.next = reverseBetween(head.next, left - 1, right - 1);
+        return head;
+    }
+
+    public ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            nextNode = head.next;
+            return head;
+        }
+        ListNode last = reverseN(head.next, n - 1);
+        head.next.next = head;
+        head.next = nextNode;
+        return last;
+    }
+}
