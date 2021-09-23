f42807978e5b9086347be1b92e2128e3ce763580
nfx_git <13561592791@163.com>
2021-09-23 16:00:01 +0800

    回文链表

---
+++ src/isPalindrome.java
@@ -0,0 +1,37 @@
+// 234.回文链表
+public class isPalindrome {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow, fast;
+        slow = fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        ListNode left = head;
+        ListNode right = reverse(slow);
+        while (right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode cur, pre, next;
+        pre = null;
+        cur = next = head;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
