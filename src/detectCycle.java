edcdbc430939fd179cc58d04def2055a2e1e1a66
nfx_git <13561592791@163.com>
2021-09-24 16:35:27 +0800

    环形链表II

---
+++ src/detectCycle.java
@@ -0,0 +1,23 @@
+// 142.环形链表II
+public class detectCycle {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow, fast;
+        slow = fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
