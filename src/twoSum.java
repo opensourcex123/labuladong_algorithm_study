d6dbcdbd4b4d2ccf065c2077eb6a827433dd3221
nfx_git <13561592791@163.com>
2021-09-25 19:42:33 +0800

    两数之和 II - 输入有序数组

---
+++ src/twoSum.java
@@ -0,0 +1,18 @@
+// 167. 两数之和 II - 输入有序数组
+public class twoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left <= right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
