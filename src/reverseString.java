7c16cdc6a8fb08ce6b03de8c579c0c2702a42211
nfx_git <13561592791@163.com>
2021-09-25 19:45:51 +0800

    反转字符串

---
+++ src/reverseString.java
@@ -0,0 +1,14 @@
+// 344.反转字符串
+public class reverseString {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left <= right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
