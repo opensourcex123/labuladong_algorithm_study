60cdf45b43e42c8bf7b575321de18042e36d1db0
nfx_git <13561592791@163.com>
2021-10-02 18:05:44 +0800

    栈实现队列

---
+++ src/MyQueue.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+// 232.用栈实现队列
+public class MyQueue {
+    Stack<Integer> s1, s2;
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return s2.pop();
+    }
+
+    public int peek() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.peek();
+    }
+
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
