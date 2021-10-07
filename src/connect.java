1a84c111130a6382bed4dcc8f2dc15763a0b5699
nfx_git <13561592791@163.com>
2021-10-07 09:05:23 +0800

    填充每个节点的下一个右侧节点指针

---
+++ src/connect.java
@@ -0,0 +1,36 @@
+// 116.填充每个节点的下一个右侧节点指针
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+public class connect {
+    public Node connect(Node root) {
+        if (root == null) return null;
+        connectTwoNode(root.left, root.right);
+        return root;
+    }
+
+    public void connectTwoNode(Node node1, Node node2) {
+        if (node1 == null || node2 == null) return;
+        node1.next = node2;
+
+        connectTwoNode(node1.left, node1.right);
+        connectTwoNode(node2.left, node2.right);
+        connectTwoNode(node1.right, node2.left);
+    }
+}
