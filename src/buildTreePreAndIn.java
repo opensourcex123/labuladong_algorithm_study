f5432437c03ae5dc21388e716a0c371bc644b9dc
nfx_git <13561592791@163.com>
2021-10-10 13:21:42 +0800

    从前序和中序遍历序列构造二叉树

---
+++ src/buildTreePreAndIn.java
@@ -0,0 +1,24 @@
+// 105.从前序和中序遍历序列构造二叉树
+public class buildTreePreAndIn {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+    }
+
+    public TreeNode build(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
+        if (preStart > preEnd || inStart > inEnd) return null;
+
+        int rootVal = preorder[preStart];
+        int index = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == rootVal) {
+                index = i;
+                break;
+            }
+        }
+        int leftSize = index - inStart;
+        TreeNode root = new TreeNode(rootVal);
+        root.left = build(preorder, preStart + 1, preStart + leftSize, inorder, inStart, index - 1);
+        root.right = build(preorder, preStart + leftSize + 1, preEnd, inorder, index + 1, inEnd);
+        return root;
+    }
+}
