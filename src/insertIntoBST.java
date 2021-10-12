bb9adf79d17e6c2a1d69b6715f1be067807f9254
nfx_git <13561592791@163.com>
2021-10-12 19:52:15 +0800

    BST的插入

---
+++ src/insertIntoBST.java
@@ -0,0 +1,13 @@
+// 701.二叉搜索树中的插入操作
+public class insertIntoBST {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        }
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
