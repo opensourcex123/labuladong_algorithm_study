c84e2d0aa0ccfe26b41805eae3ae11b6d78be277
nfx_git <13561592791@163.com>
2021-10-12 19:24:44 +0800

    判断BST是否合法

---
+++ src/isValidBST.java
@@ -0,0 +1,15 @@
+// 是否是合法的二叉树
+public class isValidBST {
+    public boolean isValidBST(TreeNode root) {
+        return isBST(root, null, null);
+    }
+
+    public boolean isBST(TreeNode root, TreeNode min, TreeNode max) {
+        if (root == null) return true;
+
+        if (max != null && root.val >= max.val) return false;
+        if (min != null && root.val <= min.val) return false;
+
+        return isBST(root.left, min, root) && isBST(root.right, root, max);
+    }
+}
