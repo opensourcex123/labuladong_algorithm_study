4e3e48c8cf0ed77a624cab20e67da02eb74d047c
nfx_git <13561592791@163.com>
2021-10-12 19:57:25 +0800

    BST的删除

---
+++ src/deleteNode.java
@@ -0,0 +1,24 @@
+// 450.删除BST中的节点
+public class deleteNode {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        if (root.val == key) {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+
+            TreeNode minNode = getMin(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, minNode.val);
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            root.right = deleteNode(root.right, key);
+        }
+        return root;
+    }
+
+    public TreeNode getMin(TreeNode root) {
+        while (root.left != null) root = root.left;
+        return root;
+    }
+}
