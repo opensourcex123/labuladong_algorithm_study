61a69f3f6090f346077f8512c06fdca10368bf3a
nfx_git <13561592791@163.com>
2021-10-17 19:40:39 +0800

    无向图中的连通分量数目

---
+++ src/UF.java
@@ -0,0 +1,50 @@
+// 323.无向图中的连通分量数目 Union-Find算法，解决无向图问题
+public class UF {
+
+    private int count;
+    private int[] size;
+    private int[] parent;
+
+    public UF(int n) {
+        this.count = n;
+        size = new int[n];
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+
+        if (size[rootP] > size[rootQ]) {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        } else if (size[rootP] < size[rootQ]) {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+        count--;
+    }
+
+    public boolean connected(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        return rootP == rootQ;
+    }
+
+    private int find(int x) {
+        while (parent[x] != x) {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    }
+
+    public int count() {
+        return count;
+    }
+}
