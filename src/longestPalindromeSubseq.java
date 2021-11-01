40d1f8cbd0bd290fe359468329b047d88d997e06
nfx_git <13561592791@163.com>
2021-11-01 15:41:33 +0800

    最长回文子序列

---
+++ src/longestPalindromeSubseq.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+// 516.最长回文子序列
+public class longestPalindromeSubseq {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int[] row : dp) {
+            Arrays.fill(row, 0);
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j]);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+}
