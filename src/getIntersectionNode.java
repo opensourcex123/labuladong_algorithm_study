3c3a7a58f7fdba58de22e08e9dfad0a7478450c4
nfx_git <13561592791@163.com>
2021-11-08 16:19:39 +0800

    相交链表

---
+++ src/getIntersectionNode.java
@@ -0,0 +1,21 @@
+// 160.相交链表
+public class getIntersectionNode {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while (p1 != p2) {
+            if (p1 == null) {
+                p1 = headB;
+            } else {
+                p1 = p1.next;
+            }
+
+            if (p2 == null) {
+                p2 = headA;
+            } else {
+                p2 = p2.next;
+            }
+        }
+        return p1;
+    }
+}
