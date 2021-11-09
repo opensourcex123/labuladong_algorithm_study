222347c39ebdcbd117c40417010f35c29cba748b
nfx_git <13561592791@163.com>
2021-11-09 16:10:05 +0800

    统计封闭岛屿的数目

---
+++ src/closedIsland.java
@@ -0,0 +1,41 @@
+// 1254.统计封闭岛屿的数目
+public class closedIsland {
+    public int closedIsland(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int res = 0;
+
+        // 把四周的岛屿排除掉，剩下的就是封闭岛屿
+        for (int i = 0; i < m; i++) {
+            dfs(grid, i, 0);
+            dfs(grid, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(grid, 0, j);
+            dfs(grid, m - 1, j);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    void dfs(int[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+        if (i < 0 || j < 0 || i >= m || j >= n) return;
+        if (grid[i][j] == 1) return;
+        grid[i][j] = 1;
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+    }
+}
