ac20838e19eb754decbe205a2e10fb2f319eb38c
nfx_git <13561592791@163.com>
2021-11-23 09:05:43 +0800

    计数质数

---
+++ src/countPrimes.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+// 204.计数质数
+public class countPrimes {
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+
+        return count;
+    }
+}
