e414eaa56c69b3077a5ab79ddd368917b5a3ed39
nfx_git <13561592791@163.com>
2021-11-24 15:41:22 +0800

    买卖股票的最佳时机

---
+++ src/maxProfit.java
@@ -0,0 +1,20 @@
+// 121.买卖股票的最佳时机
+public class maxProfit {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            if (i - 1 < 0) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+                continue;
+            }
+
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        }
+
+        return dp[n - 1][0];
+    }
+}
