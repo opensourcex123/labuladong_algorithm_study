713b36c48a61ebb205a957ceaad511b937aecf1d
nfx_git <13561592791@163.com>
2021-12-09 18:39:19 +0800

    二维区域和检索-矩阵不可变

---
+++ src/NumMatrix.java
@@ -0,0 +1,20 @@
+// 304.二维区域和检索-矩阵不可变
+public class NumMatrix {
+    private int[][] preSum;
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        preSum = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                preSum[i][j] = preSum[i-1][j] + preSum[i][j-1] + matrix[i - 1][j - 1] - preSum[i-1][j-1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return preSum[row2+1][col2+1] - preSum[row1][col2+1] - preSum[row2+1][col1] + preSum[row1][col1];
+    }
+}
