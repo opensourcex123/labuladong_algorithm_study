1164129822cf7e78f1637aa831711dbf58e0f22a
nfx_git <13561592791@163.com>
2021-12-14 15:58:40 +0800

    拼车 + 优势洗牌

--- src/advantageCount.java
+++ src/advantageCount.java
@@ -33,7 +33,6 @@ public class advantageCount {
                 left++;
             }
         }
-
         return res;
     }
 }
--- src/carPooling.java
+++ src/carPooling.java
@@ -10,6 +10,7 @@ public class carPooling {
         for (int re : res) {
             if (re > capacity) return false;
         }
+
         return true;
     }
 }
