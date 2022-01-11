2881816b836de757a5a2c144f5a9d29ba4c6c788
nfx_git <13561592791@163.com>
2022-01-11 21:48:48 +0800

    最长回文子串

---
+++ src/longestPalindrome.java
@@ -0,0 +1,25 @@
+// 5.最长回文子串
+public class longestPalindrome {
+    public String longestPalindrome(String s) {
+        String res = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            String s1 = palindrome(s, i, i);
+            String s2 = palindrome(s, i, i + 1);
+
+            res = res.length() > s1.length() ? res : s1;
+            res = res.length() > s2.length() ? res : s2;
+        }
+
+        return res;
+    }
+
+    private String palindrome(String s, int i, int j) {
+        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+            i--;
+            j++;
+        }
+
+        return s.substring(i + 1, j);
+    }
+}
